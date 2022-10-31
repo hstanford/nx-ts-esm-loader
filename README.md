c181613859b2a2fc9cf97e90434939de112245e5
Henry Stanford <hst@nford.me>
2022-10-31 12:45:28 +0000

    feat: add loader

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # nx-ts-esm-loader
-Module loader to allow running ESM-targeted node scripts importing from libraries in an NX workspace
+
+Module loader to allow running ESM-targeted node scripts importing from libraries in an NX workspace.
+
+A thin wrapper around ts-node's esm module loader.
+
+## Usage
+
+```bash
+TS_NODE_PROJECT=<path_to_nx_library_or_app>/tsconfig.lib.json  NX_BASE_TSCONFIG=<path_to_nx_workspace>/tsconfig.base.json node --loader ./loader.mjs <script_to_run>
+```
---
+++ loader.mjs
@@ -0,0 +1,41 @@
+import { createRequire, _resolveFilename } from "module";
+import { dirname } from "path";
+import { pathToFileURL, fileURLToPath } from "url";
+import { resolve as tsNodeResolve } from "ts-node/esm";
+
+export { load, getFormat, transformSource } from "ts-node/esm";
+
+const require = createRequire(import.meta.url);
+
+let otherPaths = [];
+let nxBaseDir = "";
+// load the tsconfig, grab the library paths,
+// and get the base nx workspace directory
+if (process.env.NX_BASE_TSCONFIG) {
+  const tsconfig = require(process.env.NX_BASE_TSCONFIG);
+
+  otherPaths = tsconfig.compilerOptions.paths;
+
+  const baseConfigURL = pathToFileURL(process.env.NX_BASE_TSCONFIG).href;
+  nxBaseDir = dirname(baseConfigURL) + "/";
+}
+
+export async function resolve(specifier, context, nextResolve) {
+  let refinedSpecifier = specifier;
+
+  // use NX's lib paths to resolve local packages
+  if (otherPaths[specifier]) {
+    refinedSpecifier = new URL(otherPaths[specifier], nxBaseDir).href;
+  }
+
+  // if there's no extension and the path is relative
+  // try using commonjs's loader to find the file path
+  if (!/\.[cm]?[jt]s$/.test(specifier) && /^\.\.?\//.test(specifier)) {
+    const path = dirname(fileURLToPath(context.parentURL));
+    refinedSpecifier = require.resolve(specifier, {
+      paths: [path],
+    });
+  }
+
+  return await tsNodeResolve(refinedSpecifier, context, nextResolve);
+}
